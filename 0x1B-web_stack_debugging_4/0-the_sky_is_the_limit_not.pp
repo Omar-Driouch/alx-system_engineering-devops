6de31fdc58b90a1a885d06f648fa8451bbda3c2b
Omar-Driouch <omardriouch29@gmail.com>
2024-03-14 12:38:40 +0000

    update

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+hanges /etc/default/nginx file to increase limit
+# current nginx request open files
+
+exec {'Increase nginx limit':
+  command => "sed -i 's/15/10000/' /etc/default/nginx; sudo service nginx restart",
+  path    => '/usr/bin/:/usr/local/bin/:/bin/'
+}
