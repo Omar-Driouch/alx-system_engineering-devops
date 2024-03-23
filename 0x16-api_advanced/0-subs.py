c9496cd3a4a13c5c873e75a1cdcf7047e575bb37
Omar-Driouch <omardriouch29@gmail.com>
2024-03-23 13:01:29 +0000

    up

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,14 +3,13 @@
 import requests
 
 
+
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    req = requests.get(url, headers={'User-Agent': 'Python/requests'})
+
+    try:
+        resp = req.json()
+        return resp.get("data", {}).get("subscribers", 0)
+    except Exception:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
